cf1a03547ef9fd1b8913139ec6d007e790f5ee90
lehuutuong2027@gmail.com <lehuutuong2027@gmail.com>
2022-04-09 20:23:34 +0700

    Detailed Invoice

--- resources/views/admin/detailed_invoice.blade.php
+++ resources/views/admin/detailed_invoice.blade.php
@@ -8,7 +8,7 @@
                 <div class="card my-4">
                     <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                         <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                            <h6 class="text-white text-capitalize ps-3">Chi tiết hóa đơn - <?= $id_invoice ?></h6>
+                            <h6 class="text-white text-capitalize ps-3">Chi tiết hóa đơn _- <?= $id_invoice ?></h6>
                         </div>
                     </div>
 
