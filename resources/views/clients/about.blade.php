b41c8e8371331a0667d688c980016d6b374eaefa
lehuutuong2027@gmail.com <lehuutuong2027@gmail.com>
2022-04-09 18:53:59 +0700

    Display product

--- resources/views/clients/about.blade.php
+++ resources/views/clients/about.blade.php
@@ -27,7 +27,7 @@
                 <div class="col-lg-6">
                     <h2 class="noo-sh-title-top">Chúng tôi là <span>10RealShop</span></h2>
                     <p> Sứ mệnh của 10RealShop là luôn sáng tạo và nỗ lực không ngừng để thiết kế, thi công và sản xuất  tạo nên không gian nội thất chung cư, căn hộ và biệt thự mang phong cách độc đáo, thân thiện và biến ngôi nhà như một resort 5 sao nghỉ dưỡng tại nhà. Kết hợp các giải pháp thi công tiên tiến và ứng dụng những loại vật liệu bền, đẹp trong nước và nhập khẩu, góp phần tạo nên những không gian sống và làm việc sang trọng, tiện nghi cho cộng đồng.</p>
-                    <p>Chúng tôi quyết tâm xây dựng 10RealShop trở thành một thương hiệu mạnh trong lĩnh vực tư vấn, thiết kế và thi công nội thất công trình tại Việt Nam, dựa trên tầm nhìn dài hạn, giá trị cốt lõi, chiến lược kinh doanh bài bản và hiệu quả, dựa trên nền tảng công nghệ tiên tiến cùng mô hình quản trị hiện đại và đội ngũ nhân sự chuyên nghiệp.</p>
+                    <p>Chúng tôi quyết tâm xây dựng một 10RealShop trở thành một thương hiệu mạnh trong lĩnh vực tư vấn, thiết kế và thi công nội thất công trình tại Việt Nam, dựa trên tầm nhìn dài hạn, giá trị cốt lõi, chiến lược kinh doanh bài bản và hiệu quả, dựa trên nền tảng công nghệ tiên tiến cùng mô hình quản trị hiện đại và đội ngũ nhân sự chuyên nghiệp.</p>
 
                 </div>
             </div>
